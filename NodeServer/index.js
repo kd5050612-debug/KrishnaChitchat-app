380a75f191377eb64db89fb4755d66ab30933d17
Krishna Das <kd5050612@gmail.com>
2026-09-05 19:09:37 +0530

    Refactor Socket.io initialization and event handling

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,10 +1,6 @@
-
-const PORT = process.env.PORT || 8000;
-
-const io = require('socket.io')(PORT, {
+const io = require('socket.io')(8000, {
     cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
+        origin: "*"
     }
 });
 
@@ -14,13 +10,11 @@ io.on('connection', socket => {
     socket.on('new-user-joined', name => {
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
-    });
-    
+    })
     socket.on('send', message => {
         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
     });
-    
-    socket.on('disconnect', () => {
+      socket.on('disconnect', () => {
         if (users[socket.id]) {
             socket.broadcast.emit('left', users[socket.id]);
             delete users[socket.id]; 
