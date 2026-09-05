6692d8e1c565820458670e6e5a40b1a993ab9d0f
Krishna Das <kd5050612@gmail.com>
2026-09-05 19:33:45 +0530

    Refactor Node.js server with Express and Socket.IO

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,36 +1,130 @@
-const io = require('socket.io')(process.env.PORT || 8000, {
+```javascript
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
     cors: {
-        origin: "*"
+        origin: "https://krishna-chitchat-app.vercel.app",
+        methods: ["GET", "POST"],
+        credentials: false
     }
 });
 
 const users = {};
 
-io.on('connection', socket => {
 
-    socket.on('new-user-joined', name => {
+/*
+    HEALTH CHECK
+*/
+
+app.get("/", (req, res) => {
+
+    res.status(200).send(
+        "Krishna-ChitChat Socket.IO Server is running"
+    );
+
+});
+
+
+/*
+    SOCKET CONNECTION
+*/
+
+io.on("connection", (socket) => {
+
+    console.log(
+        "New connection:",
+        socket.id
+    );
+
+
+    /*
+        NEW USER
+    */
+
+    socket.on("new-user-joined", (name) => {
+
         users[socket.id] = name;
 
-        socket.broadcast.emit('user-joined', name);
+        console.log(
+            `${name} joined the chat`
+        );
+
+        socket.broadcast.emit(
+            "user-joined",
+            name
+        );
+
     });
 
-    socket.on('send', message => {
-        socket.broadcast.emit('receive', {
-            message: message,
-            name: users[socket.id]
-        });
+
+    /*
+        SEND MESSAGE
+    */
+
+    socket.on("send", (message) => {
+
+        const name = users[socket.id];
+
+        console.log(
+            `${name}: ${message}`
+        );
+
+        socket.broadcast.emit(
+            "receive",
+            {
+                name: name,
+                message: message
+            }
+        );
+
     });
 
-    socket.on('disconnect', () => {
 
-        if (users[socket.id]) {
+    /*
+        USER DISCONNECTED
+    */
+
+    socket.on("disconnect", () => {
+
+        const name = users[socket.id];
+
+        if (name) {
+
+            console.log(
+                `${name} left the chat`
+            );
 
             socket.broadcast.emit(
-                'left',
-                users[socket.id]
+                "left",
+                name
             );
 
             delete users[socket.id];
+
         }
+
     });
+
 });
+
+
+/*
+    RENDER PORT
+*/
+
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+
+    console.log(
+        `Server running on port ${PORT}`
+    );
+
+});
+```
