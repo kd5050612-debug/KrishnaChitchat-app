e33df35a9a36c765fcda5ab1e4cfd5d73fa538f3
Krishna Das <kd5050612@gmail.com>
2026-09-05 19:27:52 +0530

    Refactor socket.io server to use environment port

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,4 +1,4 @@
-const io = require('socket.io')(8000, {
+const io = require('socket.io')(process.env.PORT || 8000, {
     cors: {
         origin: "*"
     }
@@ -7,17 +7,30 @@ const io = require('socket.io')(8000, {
 const users = {};
 
 io.on('connection', socket => {
+
     socket.on('new-user-joined', name => {
         users[socket.id] = name;
+
         socket.broadcast.emit('user-joined', name);
-    })
-    socket.on('send', message => {
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
     });
-      socket.on('disconnect', () => {
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', {
+            message: message,
+            name: users[socket.id]
+        });
+    });
+
+    socket.on('disconnect', () => {
+
         if (users[socket.id]) {
-            socket.broadcast.emit('left', users[socket.id]);
-            delete users[socket.id]; 
+
+            socket.broadcast.emit(
+                'left',
+                users[socket.id]
+            );
+
+            delete users[socket.id];
         }
     });
 });
